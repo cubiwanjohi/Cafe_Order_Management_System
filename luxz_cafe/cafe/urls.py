dbdcf510a9561b59a6f68429902d484c03eba614
Peter cubi <petercubi01@gmail.com>
2026-03-09 13:05:39 +0300

    Add apis

--- luxz_cafe/cafe/urls.py
+++ luxz_cafe/cafe/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('category/<int:category_id>/', views.category_items, name='category_items'),
     path('add-category/', views.add_category, name='add_category'),
-
+    path('register/', views.register, name='register'),
     path('api/categories/', views.CategoryListAPI.as_view(), name='api-categories'),
     path('api/menu/', views.MenuItemListAPI.as_view(), name='api-menu'),
     path('api/orders/', views.OrderListAPI.as_view(), name='api-orders'),
